5939b36436db2460712884ad22c28ec66c579443
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-14 13:30:27 -0500

    Add initial Home component and route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//paginas y componentes
+import { Home } from './pages/Home'
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
