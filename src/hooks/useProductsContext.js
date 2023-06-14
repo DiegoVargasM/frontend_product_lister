1cfe026f88d71d8473ed49a24abe5fbd6dae9b99
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-14 17:34:40 -0500

    Create custom hook for productscontext

---
+++ src/hooks/useProductsContext.js
@@ -0,0 +1,13 @@
+import { ProductsContext } from "../context/ProductContext";
+import { useContext } from "react";
+
+export const useProductsContext = () => {
+  const context = useContext(ProductsContext);
+
+  if (!context) {
+    throw new Error(
+      "useProductsContext must be used inside a ProductsContextProvider"
+    );
+  }
+  return context;
+};
