1760bdac710bcf5bc6d36b449afb02483efd8b9e
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-14 14:01:57 -0500

    Add products fetch and display

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("/api/products");
+      const json = await response.json();
+
+      if (response.ok) {
+        setProducts(json);
+      }
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div className="home">
+      <div className="products">
+        {products &&
+          products.map((product) => (
+            <p key={product._id}>{product.product_name}</p>
+          ))}
+      </div>
+    </div>
+  );
 };
