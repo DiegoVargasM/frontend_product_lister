a44e5a9b22c10d74927d42bee79bdf4cd0d4f29b
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-19 10:59:21 -0500

    Sync local storage with auth context on reload

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -17,6 +17,16 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
+  //check if user is logged in upon first render
+  useEffect(() => {
+    //transform jsonString to JSON object
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
 
   console.log("AuthContext state: ", state);
 
