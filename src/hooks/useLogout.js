24c5cdcc7de35747d729cb0f1439c08f0bffda93
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-19 12:32:35 -0500

    Clear products state on logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from "./useAuthContext";
+import { useProductsContext } from "./useProductsContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: productsDispatch } = useProductsContext();
 
   const logout = () => {
     // remove user from local storage
@@ -9,6 +11,9 @@ export const useLogout = () => {
 
     // remove user from auth context
     dispatch({ type: "LOGOUT" });
+
+    // remove products from products context
+    productsDispatch({ type: "SET_PRODUCTS", payload: null });
   };
   return { logout };
 };
