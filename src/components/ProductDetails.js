5c4ba89d5014b0e73e7d5beaadc008dc5bbb522d
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-09-15 17:33:27 -0500

    Add product edit functionality

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,10 +1,11 @@
 import { useProductsContext } from "../hooks/useProductsContext";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import ProductEditForm from "./ProductEditForm";
 
 const ProductDetails = ({ product }) => {
-  const navigate = useNavigate();
+  const [modal, setModal] = useState(false);
   const { dispatch } = useProductsContext();
   const { user } = useAuthContext();
 
@@ -24,46 +25,53 @@ const ProductDetails = ({ product }) => {
     }
   };
 
-  const handleEdit = async () => {
-    if (!user) {
-      return;
-    }
-    const response = await fetch(`/api/products/${product._id}`, {
-      method: "PATCH",
-      headers: {
-        Authorization: `Bearer ${user.token}`,
-      },
-    });
-    const json = await response.json();
-    if (response.ok) {
-      dispatch({ type: "EDIT_PRODUCT", payload: json });
-    }
+  const toggleEdit = () => {
+    setModal(!modal);
   };
 
   return (
-    <div className="product-details">
-      <h4>{product.product_name}</h4>
-      <p>
-        <strong>Category: </strong>
-        {product.category}
-      </p>
-      <p>
-        <strong>Amount: </strong>
-        {product.amount}
-      </p>
-      <p>
-        <strong>Aditional Info: </strong>
-        {product.aditional_info}
-      </p>
-      <p>
-        {formatDistanceToNow(new Date(product.createdAt), { addSuffix: true })}
-      </p>
-      <span className="material-symbols-outlined delete" onClick={handleDelete}>
-        delete
-      </span>
-      <span className="material-symbols-outlined edit" onClick={handleEdit}>
-        edit
-      </span>
+    <div>
+      <div className="product-details">
+        <h4>{product.product_name}</h4>
+        <p>
+          <strong>Category: </strong>
+          {product.category}
+        </p>
+        <p>
+          <strong>Amount: </strong>
+          {product.amount}
+        </p>
+        <p>
+          <strong>Aditional Info: </strong>
+          {product.aditional_info}
+        </p>
+        <p>
+          {formatDistanceToNow(new Date(product.createdAt), {
+            addSuffix: true,
+          })}
+        </p>
+        <span
+          className="material-symbols-outlined delete"
+          onClick={handleDelete}
+        >
+          delete
+        </span>
+        <span className="material-symbols-outlined edit" onClick={toggleEdit}>
+          edit
+        </span>
+      </div>
+      {modal && (
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="modal-content">
+            <ProductEditForm
+              product={product}
+              toggleEdit={toggleEdit}
+              productId={product._id}
+            ></ProductEditForm>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/ProductEditForm.js
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { useProductsContext } from "../hooks/useProductsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+const ProductEditForm = ({ product, toggleEdit, productId }) => {
+  const { dispatch } = useProductsContext();
+  const { user } = useAuthContext();
+
+  const [product_name, setProductName] = useState(product.product_name);
+  const [category, setCategory] = useState(product.category);
+  const [amount, setAmount] = useState(product.amount);
+  const [aditional_info, setAditionalInfo] = useState(product.aditional_info);
+  const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!user) {
+      setError("You must be logged in to add a product");
+      return;
+    }
+
+    const product = { product_name, category, amount, aditional_info };
+    if (product.amount < 0) {
+      setError("Amount debe ser mayor a 0");
+      return;
+    }
+    // la constante response es un objeto del tipo response:
+    // contiene informacion sobre la respuesta HTTP
+    // acceso a metodos como response.ok, .status, .headers, .json()
+    const response = await fetch(`/api/products/${productId}`, {
+      method: "PATCH",
+      // no podemos enviar un objeto, por eso lo convertimos a JSON string
+      body: JSON.stringify(product),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    // creamos una variable json para guardar la respuesta
+    const json = await response.json();
+    console.log("RESPONSE de edit", response);
+    console.log("json de edit", json);
+    console.log(product._id);
+    console.log(productId);
+
+    if (!response.ok) {
+      // en el controlador creamos la propiedad error
+      setError(json.error);
+      // validacion de campos vacios
+      setEmptyFields(json.emptyFields);
+    }
+    if (response.ok) {
+      setProductName("");
+      setCategory("");
+      setAmount("");
+      setAditionalInfo("");
+      setError(null);
+      setEmptyFields([]);
+      toggleEdit();
+      //dispatch({ type: "EDIT_PRODUCT", payload: json.product, productId });
+    }
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Edit Expense</h3>
+
+      <label>Title:</label>
+      <input
+        type="text"
+        onChange={(e) => setProductName(e.target.value)}
+        value={product_name}
+        className={emptyFields.includes("Title") ? "error" : ""}
+      />
+
+      <label>Category:</label>
+      <input
+        type="text"
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
+        className={emptyFields.includes("Category") ? "error" : ""}
+      />
+
+      <label>Amount:</label>
+      <input
+        type="number"
+        onChange={(e) => setAmount(e.target.value)}
+        value={amount}
+        className={emptyFields.includes("Amount") ? "error" : ""}
+      />
+
+      <label>Aditional Info:</label>
+      <textarea
+        type="text"
+        onChange={(e) => setAditionalInfo(e.target.value)}
+        value={aditional_info}
+      />
+
+      <button>Edit</button>
+      <br />
+      <br />
+      <button onClick={toggleEdit}>Cancel</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default ProductEditForm;
